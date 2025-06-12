257e465bea192dc9490a3ab41f115c441e68f911
Sharp0802 <sharp0802.business@gmail.com>
2025-06-12 23:16:30 +0900

    :sparkles: Create microbenchmark

--- test/main.cxx
+++ test/main.cxx
@@ -1,40 +1,67 @@
-#include "tasksys/tasksys.h"
-#include "tasksys/workqueue.h"
+#include "tasksys/Worker.h"
 #include <iostream>
 
-ts::Task<int> compute() {
-  co_return 100;
-}
+#define BENCHMARK_SAMPLE 10000
+#define WARMUP_SAMPLE 100
 
 void task(void *data) {
   volatile bool *p = static_cast<bool *>(data);
   *p = true;
 }
 
-__attribute__((optimize("O0")))
-void tp_benchmark(ts::ThreadPool& tp, long *t) {
-  volatile bool p;
+[[clang::optnone]]
+void LocalQueue__push_pop(void *p) {
+  auto *buffer = static_cast<ts::LocalQueue *>(p);
 
+  ts::Job job{ task, nullptr };
+
+  assert(buffer->push(job));
+  assert(buffer->pop(&job));
+}
+
+[[clang::optnone]]
+void GlobalQueue__push_pop(void *p) {
+  auto *buffer = static_cast<ts::GlobalQueue *>(p);
+
+  ts::Job job{ task, nullptr };
+
+  buffer->push(job);
+  assert(buffer->pop(&job));
+}
+
+[[clang::optnone]]
+void WorkerGroup__push(void *p) {
+  auto *pool = static_cast<ts::WorkerGroup *>(p);
+
+  auto b = false;
+  ts::Job job{ task, &b };
+  pool->push(job);
+  while (!b) {}
+}
+
+[[clang::optnone]]
+void benchmark(const char *name, void (*fn)(void *), void *p, int sample = BENCHMARK_SAMPLE) {
   auto begin = std::chrono::high_resolution_clock::now();
-  tp.push(task, (void *) &p);
-  while (!p) {}
+  for (auto i = 0; i < sample; ++i) {
+    fn(p);
+  }
   std::chrono::nanoseconds elapsed = std::chrono::high_resolution_clock::now() - begin;
-
-  *t += duration_cast<std::chrono::nanoseconds>(elapsed).count();
+  if (name != nullptr) {
+    std::cout << name << "\t: " << (elapsed.count() / sample) << "ns\n";
+  }
 }
 
 int main() {
+  ts::LocalQueue local{64 };
+  benchmark(nullptr, LocalQueue__push_pop, &local, WARMUP_SAMPLE);
+  benchmark("local-queue", LocalQueue__push_pop, &local);
 
-  ts::ThreadPool tp{2, 128};
+  ts::GlobalQueue global;
+  benchmark(nullptr, GlobalQueue__push_pop, &global, WARMUP_SAMPLE);
+  benchmark("global-queue", GlobalQueue__push_pop, &global);
 
-  long t = 0;
-  tp_benchmark(tp, &t); // warm-up
-  t = 0;
-
-  for (int i = 0; i < 1000000; ++i) {
-    tp_benchmark(tp, &t);
-  }
-
-  std::cout << (t / 1000000) << std::flush;
-  tp.stop();
+  ts::WorkerGroup pool{ 4, 64 };
+  benchmark(nullptr, WorkerGroup__push, &pool, WARMUP_SAMPLE);
+  benchmark("thread-pool", WorkerGroup__push, &pool);
+  pool.stop();
 }
