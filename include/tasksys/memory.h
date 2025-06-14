0a7cc803c6cde3047232080ebd50068fd40da35d
Sharp0802 <sharp0802.business@gmail.com>
2025-06-14 15:25:26 +0900

    :safety_vest: Check (de)allocation error

--- include/tasksys/memory.h
+++ include/tasksys/memory.h
@@ -6,11 +6,22 @@
 namespace ts {
   template<typename T>
   T *alloc(std::size_t size) {
-    return static_cast<T *>(operator new(sizeof(T) * size, std::align_val_t{alignof(T)}));
+    auto *p = static_cast<T *>(operator new(sizeof(T) * size, std::align_val_t{alignof(T)}));
+#if _DEBUG
+    if (p == nullptr) {
+      throw std::runtime_error("allocation failed!");
+    }
+#endif
+    return p;
   }
 
   template<typename T>
   void free(T *p) {
+#if _DEBUG
+    if (p == nullptr) {
+      return;
+    }
+#endif
     operator delete(p, std::align_val_t{alignof(T)});
   }
 }
