e3216154b24d324af45e89e2a8a5c3d1863fb55a
Sharp0802 <sharp0802.business@gmail.com>
2025-11-03 13:14:11 +0900

    :white_check_mark: Add tests for worker

---
+++ lib/worker.test.cxx
@@ -0,0 +1,110 @@
+#ifdef __TS_TEST__
+
+#include <atomic>
+#include <chrono>
+#include <numeric>
+#include <thread>
+#include <vector>
+#include <gtest/gtest.h>
+
+#include "ts/queue.h"
+#include "ts/worker.h"
+
+using namespace ts;
+
+static constexpr size_t THREAD_COUNT = 8;
+constexpr size_t BASE_ITEM_COUNT = 1024 * 16;
+
+// --- Test ts::worker ---
+
+TEST(WorkerTest, BasicDispatch) {
+  std::array<worker*, 1> workers{};
+  vyukov<job*> global(4096);
+
+  worker w(workers.data(), 1, global, 4096, 128);
+  workers[0] = &w;
+
+  w.start();
+
+  std::atomic_flag done = ATOMIC_FLAG_INIT;
+
+  ASSERT_TRUE(global.push(job::create([&done] (size_t) {
+    done.test_and_set();
+  }, 0, 1, nullptr)));
+
+  while (!done.test()) {
+    _mm_pause();
+  }
+
+  global.kill();
+  w.stop();
+}
+
+TEST(WorkerTest, GrainedIntegrity) {
+  std::array<worker*, THREAD_COUNT> workers{};
+  std::array<std::vector<size_t>, THREAD_COUNT> buffer{};
+
+  std::atomic_size_t counter = 0;
+  std::atomic_flag alarm = ATOMIC_FLAG_INIT;
+
+  vyukov<job*> global(4096);
+
+  // prepare workers
+  for (size_t i = 0; i < THREAD_COUNT; ++i) {
+    workers[i] = new worker(workers.data(), THREAD_COUNT, global, 4096, 128);
+  }
+  for (size_t i = 0; i < THREAD_COUNT; ++i) {
+    workers[i]->start();
+  }
+
+  // push works
+  for (size_t i = 0; i < BASE_ITEM_COUNT; ++i) {
+    const auto ci = i;
+    global.blocking_push(job::create([&buffer, &counter, &alarm, ci] (size_t) {
+      buffer[worker::current()->id()].push_back(ci);
+
+      const auto current = ++counter;
+      if (current == BASE_ITEM_COUNT) {
+        alarm.test_and_set(release);
+        alarm.notify_all();
+      }
+      if (current % 1024 == 0) {
+        std::cout << current << '/' << BASE_ITEM_COUNT << std::endl;
+      }
+    }, 0, 1, nullptr));
+  }
+
+  // waiting for completion
+  do {
+    alarm.wait(false);
+  } while (!alarm.test_and_set());
+
+  // finalization
+  global.kill();
+  for (size_t i = 0; i < THREAD_COUNT; ++i) {
+    workers[i]->stop();
+  }
+
+  // verify
+  std::set<size_t> check;
+  for (size_t i = 0; i < THREAD_COUNT; ++i) {
+    for (const auto v : buffer[i]) {
+      const auto [_, b] = check.insert(v);
+      EXPECT_TRUE(b) << "duplication found: " << v;
+    }
+  }
+  for (size_t i = 0; i < BASE_ITEM_COUNT; ++i) {
+    EXPECT_TRUE(check.contains(i)) << "missing found: " << i;
+  }
+
+  // summarize
+  for (size_t i = 0; i < THREAD_COUNT; ++i) {
+    std::cout << "thread[" << i << "] = " << buffer[i].size() << std::endl;
+  }
+
+  for (const auto worker: workers) {
+    delete worker;
+  }
+}
+
+#endif
